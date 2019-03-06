2bbb88b647f8a4276fc2f1d298e7f6e5617b0f4d
Petri Räsänen <rasanen.petri@gmail.com>
2019-03-06 15:09:48 +0200

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,12 @@
+/*
+ill add something like this later:
+  <meta property="og:type" content="website" />
+  <meta property="fb:app_id" content="126083560882910" />
+  <meta property="og:title" content="Radio en vivo | Radio Planeta" />
+  <meta property="og:url" content="https://planeta.pe/radioenvivo" />
+  <meta property="og:image" content="https://planeta.pe/assets/images/punto7___logo.jpg" />
+
+  <meta property="og:site_name" content="Radio Planeta 107.7 FM" />
+  <meta property="og:description" content="Escucha Radio Planeta 107.7 fm en vivo. Pop, Hip Hop, Rock, música actual en inglés, letras de canciones, noticias, artistas, Mañana Maldita, Oh my Gachi" />
+  <meta property="og:locale" content="es_ES" />
+*/
